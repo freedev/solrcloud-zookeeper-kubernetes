414ba594d55147903f177cfc657d09d8bc4b5a03
Antony Briggs <antony.briggs@nationwidedigital.co.uk>
2019-01-16 16:01:09 +0000

    create service account bound to cluster-admin. Create ingress controller using helm. configure ingress to use root path

--- start-aks.sh
+++ start-aks.sh
@@ -8,9 +8,13 @@ kubectl get configmap
 
 kubectl create -f services/service-solr.yml
 kubectl create -f statefulsets/statefulset-solr.yml
+
+# kubectl create -f rbac-config.yaml
+# helm init --service-account tiller
+helm install stable/nginx-ingress --namespace kube-system --set controller.replicaCount=2 --set rbac.create=false
 kubectl apply -f ingress/ingress.yml
 
 kubectl get pod
 kubectl get deployment
 kubectl get service
-
+kubectl get service -l app=nginx-ingress --namespace kube-system    # ingress IP
