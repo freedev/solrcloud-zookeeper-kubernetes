3e14b94bdbde0b1297726435e158d62a94b3d833
Antony Briggs <36103120+piratebriggs@users.noreply.github.com>
2019-04-26 10:52:03 +0100

    Scaled Solr (#5)
    * Updated stateful set to use podAntiAffinity & RollingUpdate.
    
    Added headless service so that each pod has resolvable name. updated SOLR_HOST to match.
    initial config of ingress.
    
    * add the zookeeper creation
    
    * create pods in parallel
    
    * Solr needs to know the addresses of ALL the zookeepers!
    
    * create service account bound to cluster-admin. Create ingress controller using helm. configure ingress to use root path
    
    * create tls for ingress controller
    
    * this looks like a copy-n-paste error?
    
    * add a liveness check based on whether the admin page loads. I tried using solr healthcheck but as soon as one node is down the custer state is unhealthy leading to a cascade of pod-restarts
    
              exec:
                command:
                - sh
                - -c
                - "solr healthcheck -c ping-col | grep -c status...healthy"
    
    * Updated IP as previous one was commited to public repo
    
    * Add  PDB
    
    * added note

---
+++ start-aks.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# build zookeeper ensenble from k8s repo
+kubectl apply -f https://k8s.io/examples/application/zookeeper/zookeeper.yaml
+
+kubectl create configmap solr-config --from-env-file=configmap/solr-config.properties 
+
+kubectl get configmap
+
+# I think the headless service needs creating before the stateful set
+kubectl create -f services/service-solr.yml
+kubectl create -f statefulsets/pod-disruption-budget.yml
+kubectl create -f statefulsets/statefulset-solr.yml
+
+# init helm
+# 1. create service account called tiller
+# kubectl create -f service-account/rbac-config.yaml
+# 2. init
+# helm init --service-account tiller
+
+# create ingress controller (helm)
+helm install stable/nginx-ingress --namespace kube-system --set controller.replicaCount=2 --set rbac.create=false
+
+# get eternal ip of ingress controller
+kubectl get service -l app=nginx-ingress --namespace kube-system
+
+# create dns for ip address created by ingress controller 
+
+##!/bin/bash
+## Public IP address of your ingress controller
+#IP="138.91.55.80"
+## Get the resource-id of the public ip
+#PUBLICIPID=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$IP')].[id]" --output tsv)
+## Update public ip address with DNS name
+#az network public-ip update --ids $PUBLICIPID --dns-name "nobby-a4aedadfe4"
+
+
+# create cert-manager 
+# This is a 2-step process as a work-around for some issues with CRDs
+kubectl apply \
+    -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.6/deploy/manifests/00-crds.yaml
+helm repo update
+helm install stable/cert-manager \
+    --namespace kube-system  \
+    --set ingressShim.defaultIssuerName=letsencrypt-prod  \
+    --set ingressShim.defaultIssuerKind=ClusterIssuer \
+    --set rbac.create=false \
+    --set serviceAccount.create=false \
+    --set createCustomResource=false
+
+# create cert issuer (not sure on dependency on above)
+kubectl apply -f ingress/cluster-issuer.yaml
+
+# create cert 
+kubectl apply -f ingress/certificates.yaml
+
+# create ingress - it takes a few minutes for the certificate to be visible on the https site
+kubectl apply -f ingress/ingress.yml
+
+kubectl get pod
+kubectl get statefulset
+kubectl get service
+kubectl get certificate
+kubectl get ingress
