73cebe87227cb3b0236a391b1f336e9a81e3b0a2
Vincenzo D'Amore <v.damore@gmail.com>
2019-07-08 13:16:49 +0200

    fixed aks cluster creation

--- stop-and-delete-aks-solr-cluster.sh
+++ stop-and-delete-aks-solr-cluster.sh
@@ -7,7 +7,16 @@ kubectl delete service solr-service
 
 kubectl delete pvc volsolr-solr-0
 kubectl delete pvc volsolr-solr-1
+# kubectl delete pvc volsolr-solr-2
+
+PV=$(kubectl get pv | grep "default/volsolr-solr-" | awk '{ print $1}')
+if [ "$PV" == "" ]
+then
+   echo "persistent volume not found"
+else
+   kubectl delete pv $PV
+fi
 
 kubectl delete configmap solr-cluster-config 
-
+kubectl delete sc store-solrcluster
 
--- stop-and-delete-aks-zookeeper-ensemble.sh
+++ stop-and-delete-aks-zookeeper-ensemble.sh
@@ -9,6 +9,14 @@ kubectl delete pvc volzookeeper-zk-0
 kubectl delete pvc volzookeeper-zk-1
 kubectl delete pvc volzookeeper-zk-2
 
+PV=$(kubectl get pv | grep "default/volzookeeper-zk-" | awk '{ print $1}')
+if [ "$PV" == "" ]
+then
+   echo "persistent volume not found"
+else
+   kubectl delete pv $PV
+fi
+
 kubectl delete configmap zookeeper-ensemble-config 
 
-
+kubectl delete storageclass store-zkensemble
